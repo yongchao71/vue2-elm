c80d7767fd1a7a1212dbca3236193d50a8bc532d
yongchao71 <408775927@qq.com>
2018-10-06 17:51:18 +0800

    add set meal detail

--- src/page/setmealdetail/setmealdetail.vue
+++ src/page/setmealdetail/setmealdetail.vue
@@ -1,12 +1,62 @@
 <template src="./setmealdetail.html"></template>
-
 <script src="./setmealdetail.js"></script>
-
-
 <style lang="scss" scoped>
   @import '../../style/mixin';
-  .color{
-      color: rebeccapurple;
+  .detail-ct{
+    .food-img{
+      @include wh(100%,"")
+    }
+    /******************top start************************/
+    .title-outer{
+      padding: 0.4rem;
+      .f-title-ct{
+        .title-ct{
+          flex: 1 1 5rem;
+        }
+        .share-ct{
+          flex: 0 0 2rem;
+        }
+      }
+      .rating-ct{
+        padding: 0.4rem 0;
+      }
+      .current-info{
+        background: #F0F6D4;
+        padding: 0.1rem 0.2rem;
+      }
+    }
+      /******************top end************************/
+    .shop-info-ct{
+      padding: 0.4rem;
+      color: #999999;
+      .i-b{
+        display: inline-block;
+      }
+      .shop-name{
+          width: 3rem;
+      }
+      .p-icon{
+        @include wh(0.7rem,0.7rem);
+        margin-right: 0.2rem;
+      }
+    }
+    .purchase-ct{
+      .p-price{
+         background: #B2D235;
+         flex: 0 0 5rem;
+        border-radius: 1rem 0 0 1rem;
+      }
+      .p-discount{
+         background: #7AC043;
+         flex: 1 1 5rem;
+      }
+      .p-right-now{
+          background: #003F34;
+          flex: 0 0 5rem;
+          border-radius:0 1rem 1rem 0;
+      }
+    }    
+
   }
   .icout{
    @include sc(0.8rem,red);
